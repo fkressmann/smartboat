8f0e9f75891b2310742cd20e9a5a8afeeb30516f
Felix Kressmann <felix.kressmann@boehringer-ingelheim.com>
2020-08-12 19:13:55 +0200

    fix day counter rounding and change detection

--- python/lilserver.py
+++ python/lilserver.py
@@ -31,9 +31,11 @@ class DayAggregation:
         return (time.time() - self.last_reading_time) / 3600
 
     def send_if_changed(self):
-        if round(self.aggregated_value) != round(self.previous_value):
-            self.previous_value = self.aggregated_value
-            send_to_openhab(self.get_item_name(), self.aggregated_value)
+        rounded_aggregated = round(self.aggregated_value)
+        if rounded_aggregated != self.previous_value:
+            self.previous_value = rounded_aggregated
+            print(f"Sending aggregated value {rounded_aggregated} to item {self.get_item_name()}")
+            send_to_openhab(self.get_item_name(), rounded_aggregated)
 
     def get_item_name(self):
         return self.metric + "H"
